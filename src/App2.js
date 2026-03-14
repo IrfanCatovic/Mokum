e8ebc065931f583a044c7a86cfbdaa9596842a62
IrfanCatovic <catovicc84@gmail.com>
2026-03-14 13:17:22 +0100

    Error

---
+++ src/App2.js
@@ -0,0 +1,9 @@
+import "./index.css";
+
+export default function App() {
+    return (
+        <div className="app">
+            <h1>Došlo je do greške, javite se administratoru</h1>
+        </div>
+    );
+}
